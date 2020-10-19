480e1f9b3b3d84d90d177ce5d6e484c848939b59
Arkadiusz Wydra <hello@awydra.pl>
2020-10-19 14:29:29 +0200

    Improved app stability

--- src/utils/normalizeResponse.js
+++ src/utils/normalizeResponse.js
@@ -1,18 +1,18 @@
 const normalizeResponse = (res) => {
   return res.feed.entry.map((arr) => ({
-    id: arr.id.attributes['im:id'],
-    category: arr.category.attributes.label,
-    artist: arr['im:artist'].label,
+    id: arr.id?.attributes['im:id'],
+    category: arr.category?.attributes?.label,
+    artist: arr['im:artist']?.label,
     image: arr['im:image'][2].label.replace('113x170bb.png', '460x0w.jpg'),
-    title: arr['im:name'].label,
-    price: arr['im:price'].label,
-    date: arr['im:releaseDate'].attributes.label,
+    title: arr['im:name']?.label,
+    price: arr['im:price']?.label,
+    date: arr['im:releaseDate']?.attributes?.label,
     buttons: arr.link.map((link) => ({
       title: link.attributes.title || 'Check on iTunes',
       href: link.attributes.href,
     })),
-    desc: arr.summary.label,
-    rights: arr.rights.label,
+    desc: arr.summary?.label,
+    rights: arr.rights?.label,
   }));
 };
 
